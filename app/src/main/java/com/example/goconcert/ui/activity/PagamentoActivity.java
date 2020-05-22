1f72cb6d89a3ef8f5296abda5a952d94f72466c1
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-05-22 16:28:43 -0300

    Criação das telas pagamento e resumo do compra

---
+++ app/src/main/java/com/example/goconcert/ui/activity/PagamentoActivity.java
@@ -0,0 +1,16 @@
+package com.example.goconcert.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.goconcert.R;
+
+public class PagamentoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagamento);
+    }
+}
---
+++ app/src/main/java/com/example/goconcert/ui/activity/ResumoShowActivity.java
@@ -0,0 +1,16 @@
+package com.example.goconcert.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.goconcert.R;
+
+public class ResumoShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_show);
+    }
+}
